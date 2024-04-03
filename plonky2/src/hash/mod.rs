7d3e1240be8290bc6657d1b2dc1a4b7ca65b780c
Sai Deng <deng.sai@gmail.com>
2024-04-02 19:17:25 -0700

    fmt

--- plonky2/src/hash/mod.rs
+++ plonky2/src/hash/mod.rs
@@ -2,7 +2,7 @@
 //! as well as specific hash functions implementation.
 
 mod arch;
-pub mod batch_merkle_tree;
+pub mod field_merkle_tree;
 pub mod hash_types;
 pub mod hashing;
 pub mod keccak;
@@ -13,4 +13,3 @@ pub mod poseidon;
 pub mod poseidon2;
 pub mod poseidon2_goldilocks;
 pub mod poseidon_goldilocks;
-pub mod field_merkle_tree;
