a0916a91ebe715d3b24753f63cd95350d067af57
Sai Deng <deng.sai@gmail.com>
2024-04-20 11:50:11 -0700

    build

--- plonky2/src/fri/batch_oracle.rs
+++ plonky2/src/fri/batch_oracle.rs
@@ -1,19 +1,21 @@
 #[cfg(not(feature = "std"))]
+use alloc::format;
+#[cfg(not(feature = "std"))]
 use alloc::vec::Vec;
 
 use itertools::Itertools;
 use plonky2_field::extension::Extendable;
 use plonky2_field::fft::FftRootTable;
 use plonky2_field::polynomial::{PolynomialCoeffs, PolynomialValues};
+use plonky2_field::types::Field;
 use plonky2_maybe_rayon::*;
 use plonky2_util::{log2_strict, reverse_index_bits_in_place};
-use crate::fri::batch_prover::batch_fri_proof;
-use crate::fri::FriParams;
 
+use crate::fri::batch_prover::batch_fri_proof;
 use crate::fri::oracle::PolynomialBatch;
 use crate::fri::proof::FriProof;
-use crate::fri::prover::fri_proof;
 use crate::fri::structure::{FriBatchInfo, FriInstanceInfo};
+use crate::fri::FriParams;
 use crate::hash::field_merkle_tree::FieldMerkleTree;
 use crate::hash::hash_types::RichField;
 use crate::iop::challenger::Challenger;
@@ -120,8 +122,7 @@ impl<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>
 
     /// Produces a batch opening proof.
     pub fn prove_openings(
-        initial_merkle_trees: &[&FieldMerkleTree<F, C::Hasher>],
-        instance: &[FriInstanceInfo<F, D>],
+        instances: &[FriInstanceInfo<F, D>],
         oracles: &[&Self],
         challenger: &mut Challenger<F, C::Hasher>,
         fri_params: &FriParams,
@@ -131,43 +132,51 @@ impl<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>
         let alpha = challenger.get_extension_challenge::<D>();
         let mut alpha = ReducingFactor::new(alpha);
 
-        // Final low-degree polynomial that goes into FRI.
-        let mut final_poly = PolynomialCoeffs::empty();
+        let mut final_lde_polynomial_coeff = PolynomialCoeffs::empty();
+        let mut final_lde_polynomial_values = Vec::with_capacity(instances.len());
+        for (i, instance) in instances.iter().enumerate() {
+            // Final low-degree polynomial that goes into FRI.
+            let mut final_poly = PolynomialCoeffs::empty();
 
-        // Each batch `i` consists of an opening point `z_i` and polynomials `{f_ij}_j` to be opened at that point.
-        // For each batch, we compute the composition polynomial `F_i = sum alpha^j f_ij`,
-        // where `alpha` is a random challenge in the extension field.
-        // The final polynomial is then computed as `final_poly = sum_i alpha^(k_i) (F_i(X) - F_i(z_i))/(X-z_i)`
-        // where the `k_i`s are chosen such that each power of `alpha` appears only once in the final sum.
-        // There are usually two batches for the openings at `zeta` and `g * zeta`.
-        // The oracles used in Plonky2 are given in `FRI_ORACLES` in `plonky2/src/plonk/plonk_common.rs`.
-        for FriBatchInfo { point, polynomials } in &instance.batches {
-            // Collect the coefficients of all the polynomials in `polynomials`.
-            let polys_coeff = polynomials.iter().map(|fri_poly| {
-                &oracles[fri_poly.oracle_index].polynomials[fri_poly.polynomial_index]
-            });
-            let composition_poly = timed!(
+            // Each batch `i` consists of an opening point `z_i` and polynomials `{f_ij}_j` to be opened at that point.
+            // For each batch, we compute the composition polynomial `F_i = sum alpha^j f_ij`,
+            // where `alpha` is a random challenge in the extension field.
+            // The final polynomial is then computed as `final_poly = sum_i alpha^(k_i) (F_i(X) - F_i(z_i))/(X-z_i)`
+            // where the `k_i`s are chosen such that each power of `alpha` appears only once in the final sum.
+            // There are usually two batches for the openings at `zeta` and `g * zeta`.
+            // The oracles used in Plonky2 are given in `FRI_ORACLES` in `plonky2/src/plonk/plonk_common.rs`.
+            for FriBatchInfo { point, polynomials } in &instance.batches {
+                // Collect the coefficients of all the polynomials in `polynomials`.
+                let polys_coeff = polynomials.iter().map(|fri_poly| {
+                    &oracles[fri_poly.oracle_index].polynomials[fri_poly.polynomial_index]
+                });
+                let composition_poly = timed!(
+                    timing,
+                    &format!("reduce batch of {} polynomials", polynomials.len()),
+                    alpha.reduce_polys_base(polys_coeff)
+                );
+                let mut quotient = composition_poly.divide_by_linear(*point);
+                quotient.coeffs.push(F::Extension::ZERO); // pad back to power of two
+                alpha.shift_poly(&mut final_poly);
+                final_poly += quotient;
+            }
+
+            let lde_final_poly = final_poly.lde(fri_params.config.rate_bits);
+            let lde_final_values = timed!(
                 timing,
-                &format!("reduce batch of {} polynomials", polynomials.len()),
-                alpha.reduce_polys_base(polys_coeff)
+                &format!("perform final FFT {}", lde_final_poly.len()),
+                lde_final_poly.coset_fft(F::coset_shift().into())
             );
-            let mut quotient = composition_poly.divide_by_linear(*point);
-            quotient.coeffs.push(F::Extension::ZERO); // pad back to power of two
-            alpha.shift_poly(&mut final_poly);
-            final_poly += quotient;
+            final_lde_polynomial_values.push(lde_final_values);
+            if i == 0 {
+                final_lde_polynomial_coeff = lde_final_poly;
+            }
         }
 
-        let lde_final_poly = final_poly.lde(fri_params.config.rate_bits);
-        let lde_final_values = timed!(
-            timing,
-            &format!("perform final FFT {}", lde_final_poly.len()),
-            lde_final_poly.coset_fft(F::coset_shift().into())
-        );
-
         batch_fri_proof::<F, C, D>(
-            initial_merkle_trees,
-            lde_final_poly,
-            lde_final_values,
+            &oracles.iter().map(|o|&o.field_merkle_tree).collect::<Vec<_>>(),
+            final_lde_polynomial_coeff,
+            &final_lde_polynomial_values,
             challenger,
             fri_params,
             timing,
